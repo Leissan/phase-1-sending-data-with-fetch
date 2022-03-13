d28279b9ca4f19ef7d1a2f9b4c866da5867c9692
Leissan Khan <leissan.khan@gmail.com>
2022-03-13 16:36:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+/*const formData={
+    dogName: 'Byron',
+    dogBreed: 'Poodle',
+}
+
+const configurationObject={
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+    },
+    body:JSON.stringify(formData)
+        
+};
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(res=> res.json())
+.then(object=>console.log(object))
+
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+*/
+
+
+ function submitData (userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response=> response.json())
+    .then(data=>document.body.append(data.id))
+    .catch((error)=> {
+        return document.body.append(error.message)
+      
+})
+}
+
